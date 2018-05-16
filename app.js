c0f76d877091b74cd0b131e5089fe297b8bf6c65
Michael O'Sullivan <michael@nizamimedia.com>
2018-05-16 23:38:02 +0100

    Todo list
    added in todo list and some notes in the js file.

---
+++ app.js
@@ -0,0 +1,7 @@
+// Project Rubric:
+
+// https://review.udacity.com/#!/rubrics/591/view
+
+//There should be 1 2 3 4 5 6 7 8
+
+//Check out this for a working example, http://fabiolavieyra.com/blog/memory-game
