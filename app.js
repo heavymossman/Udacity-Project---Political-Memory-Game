bd470d804e56637407ea706dae19e52813fe1da0
Michael O'Sullivan <michael@nizamimedia.com>
2018-05-17 00:54:30 +0100

    App.js
    Created the charaacter constructor function
    Created all the characters
    Tested in console

--- app.js
+++ app.js
@@ -5,3 +5,25 @@
 //There should be 1 2 3 4 5 6 7 8
 
 //Check out this for a working example, http://fabiolavieyra.com/blog/memory-game
+
+
+function Character (name, picture) {
+  this.name = name;
+  this.picture = picture;
+}
+
+const jeremy = new Character ("Jeremy Corbyn", "jeremy2.jpg");
+const tom = new Character ("Tom Watson", "jeremy2.jpg");
+const john = new Character ("John McDonnell", "jeremy2.jpg");
+const diane = new Character ("Diane Abbott", "jeremy2.jpg");
+const emily = new Character ("Emily Thornberry", "jeremy2.jpg");
+const dawn = new Character ("Dawn Butler", "jeremy2.jpg");
+const shami = new Character ("Shami Chakrabarti", "jeremy2.jpg");
+const ashworth = new Character ("Jonathan Ashworth", "jeremy2.jpg");
+
+const allCharacters = [
+  jeremy, tom, john, diane, emily, dawn, shami, ashworth
+];
+
+
+console.log(allCharacters);
