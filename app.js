0686bc15ff7ec449dbd84b049bdbcbdd91e0e761
Michael O'Sullivan <michael@nizamimedia.com>
2018-05-18 22:40:27 +0100

    Testing out different solutions

--- app.js
+++ app.js
@@ -6,7 +6,26 @@
 
 //Check out this for a working example, http://fabiolavieyra.com/blog/memory-game
 
-//Store the section id in the variable in order to generate the rows and cols
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+
+
+
+
+
 const gameArea = document.getElementById('game');
 
 function Character (name, picture) {
@@ -14,20 +33,42 @@ function Character (name, picture) {
   this.picture = picture;
 }
 
-const jeremy = new Character ("Jeremy Corbyn", "jeremy2.jpg");
-const tom = new Character ("Tom Watson", "jeremy2.jpg");
-const john = new Character ("John McDonnell", "jeremy2.jpg");
-const diane = new Character ("Diane Abbott", "jeremy2.jpg");
-const emily = new Character ("Emily Thornberry", "jeremy2.jpg");
-const dawn = new Character ("Dawn Butler", "jeremy2.jpg");
-const shami = new Character ("Shami Chakrabarti", "jeremy2.jpg");
-const ashworth = new Character ("Jonathan Ashworth", "jeremy2.jpg");
+const jeremy = new Character ("Jeremy Corbyn", "jeremy.jpg");
+const tom = new Character ("Tom Watson", "tom.jpg");
+const john = new Character ("John McDonnell", "john.jpg");
+const diane = new Character ("Diane Abbott", "diane.jpg");
+const emily = new Character ("Emily Thornberry", "emily.jpg");
+const dawn = new Character ("Dawn Butler", "dawn.jpg");
+const shami = new Character ("Shami Chakrabarti", "shami.jpg");
+const ashworth = new Character ("Jonathan Ashworth", "jonathan.jpg");
 
 const allCharacters = [
   jeremy, tom, john, diane, emily, dawn, shami, ashworth
 ];
 
+let rand = shuffle(allCharacters);
 
+console.log(rand[1])
+
+/*
+const allCharacters = {
+  Jeremy: {
+    name: "Jeremey Corbyn",
+    image: "jeremey.jpg"
+  },
+  tom: {
+    name: "Tom Watson",
+    image: "tom.jpg"
+  },
+  john: {
+    name: "John McDonnell",
+    image: "john.jpg"
+  }
+}
+
+console.log(allCharacters)
+
+*/
 
 //Build the intro row and col to store game information later
 const int = document.createElement("DIV");
@@ -52,30 +93,18 @@ function buildGame() {
 
   for (let row = 0; row < 4; row++) {
     let newRow = document.createElement("DIV");
-    newRow.className = "row text-center p-3";
+    newRow.className = "row text-center p-3 " + row;
 
     for (let col = 0; col < 4; col++){
       let newCol = document.createElement("DIV");
-      newCol.className = "col card mr-4";
+      newCol.className = "col card mr-4 " + col + row;//the last two adds col and row are just to generate a unique class name
       //Okay this is not the best for performance, but its add the toggle class function to each col
       $(newCol).click(function(evt){
             $(newCol).toggleClass("front");
       });
 
 
-    allCharacters.forEach(function(character, i) {
-        let newCharacter = document.createElement("P");
-        newCharacter.textContent = character.name;
-        console.log(newCharacter[0]);
-
-        newCol.appendChild(newCharacter);
-      });
-
-
-
-      //
-
-      //newCol.innerHTML = jeremy.name + "<br>" + jeremy.picture;
+      newCol.innerHTML = rand.name + "<br>" + rand.picture;
 
       newRow.appendChild(newCol);
 
@@ -85,3 +114,14 @@ function buildGame() {
 
   }
 }
+
+//allCharacters.forEach(function(character, i) {
+
+  //console.log(character.name)
+
+    //let newCharacter = document.createElement("P");
+    //newCharacter.textContent += character.name;
+    //console.log(newCharacter);
+
+    //newCol.append(newCharacter);
+  //});
